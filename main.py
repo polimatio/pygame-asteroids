06509fe358360b489dff4b84935888e413129fa0
Miguel A. López <15662430+polimatio@users.noreply.github.com>
2025-10-18 15:32:53 +0200

    Create Player class representing the player's ship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -15,6 +16,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2).draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Limit to 60 FPS
 
