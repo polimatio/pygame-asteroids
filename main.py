97009bf802829668ebfff6c9196673bd741edfb8
Miguel A. López <15662430+polimatio@users.noreply.github.com>
2025-10-18 14:35:32 +0200

    Limit to 60 FPS

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000  # Limit to 60 FPS
 
 
 if __name__ == "__main__":
