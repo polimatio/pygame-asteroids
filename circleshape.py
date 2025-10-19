f45da26be4ca39457ee18066660d734980a05759
Miguel A. López <15662430+polimatio@users.noreply.github.com>
2025-10-19 02:20:13 +0200

    Start organizing sprite objects into Groups

--- circleshape.py
+++ circleshape.py
@@ -3,20 +3,27 @@ import pygame
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
+        # add to sprite containers if they exist
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
 
+        # Initialize position, velocity, and radius
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
+        
+        # Groups expect sprites to have image and rect attributes
+        self.image = pygame.Surface((1, 1), pygame.SRCALPHA)
+        self.rect = self.image.get_rect(center=self.position)
 
     def draw(self, screen):
         # sub-classes must override
         pass
 
     def update(self, dt):
+        # keep rect in sync with position
+        self.rect.center = self.position
         # sub-classes must override
         pass
--- main.py
+++ main.py
@@ -4,25 +4,43 @@ from player import Player
 
 
 def main():
+    # Initialize Pygame and create the main game window
     print("Starting Asteroids!")
     pygame.init()
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    # Create sprite groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    # Create sprite instances
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # player in centre of screen
+    
+    # Game setup
     clock = pygame.time.Clock() # clock to manage frame rate
     dt = 0 # delta time between frames in seconds
+    
+    # Game loop
     while True:
-        # clock.tick(60) returns the elapsed time in ms but dt is in seconds
         dt = clock.tick(60) / 1000.0  # Limit to a frame rate of 60 FPS
+        # clock.tick(60) returns the elapsed time in ms but dt is in seconds
+        
         # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
-        pygame.display.flip()
+        
+        # Render frame
+        screen.fill("black") # clear screen each frame
+        updatable.update(dt) # update all updatable sprites
+        #drawable.draw(screen) # draw all drawable sprites
+        # custom draw to handle non-rectangular shapes
+        for sprite in drawable:
+            sprite.draw(screen) # call each sprite's draw method
+        pygame.display.flip() # update the full display surface to the screen
 
 
 if __name__ == "__main__":
